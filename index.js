7288a4c71d9043f2a9b9880807b895f62c6f269b
chris <chrishethertonweb@gmail.com>
2022-08-25 09:33:41 +0200

    initialized second exercise

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Ex 1
 
 class Product {
+
   constructor(name, price) {
+
     this.name = name
     this.price = price
   }
+
   toText() {
-    console.log(this.name, this.price, (this.price - (this.price / 1.16)));
+    console.log(this.name, this.price, (this.price - (this.price / 1.16).toFixed(2)));
   }
+
   containedVAT() {
     console.log(this.price / 1.16);
   }
@@ -17,3 +21,32 @@ const tracksuit = new Product("Adidas tracksuit", 150.0)
 const shoes = new Product("Puma running shoes", 85.99)
 const socks = new Product("Socks set", 4.99)
 
+// Ex 2
+
+class Cart {
+
+  constructor() {
+    this.products = []
+  }
+
+  addProduct(shoppedProduct) {
+    if (shoppedProduct instanceof products) {
+      this.products.push(shoppedProduct)
+      console.log(this.products.length.toString());
+    }
+    console.log(shoppedProduct, "is not available in the shop");
+  }
+
+  getProductInfoCart() {
+    toText()
+  }
+
+  toText() {
+    console.log(this.products);
+  }
+
+  getTotalItemsPrice() {
+    this.products.shoppedProduct.forEach(item => item + this.products.shoppedProduct.price)
+    console.log(item.toString());
+  }
+}
